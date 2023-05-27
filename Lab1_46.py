8ed71fa0c1003f560b35db94bd82ff2989683854
luantrancong <tcluan111@gmail.com>
2023-05-28 02:08:53 +0700

    okok

--- Lab1_46.py
+++ Lab1_46.py
@@ -1,7 +1,7 @@
 print('Bộ nghiệm của phương trình 2x+7y+9z = 979 (x,y,z)>0')
 
-for x in range((979//2)+1):
-    for y in range((979//7)+1):
-        for z in range((979//9)+1):
+for x in range(1,(979//2-7-9)+1):
+    for y in range(1,(979//7-2-9)+1):
+        for z in range(1,(979//9-2-7)+1):
             if(2*x)+(7*y)+(9*z)==979:
                 print(x,y,z)
--- Lab1_48.py
+++ Lab1_48.py
@@ -1,8 +1,8 @@
 print('Bộ nghiệm của phương trình 2x+7y+9z = 979 (x,y,z)>0 với x+y+x nho nhat')
 
-for x in range((979 // 2)+1):
-    for y in range((979 // 7) + 1):
-        for z in range((979 // 9),-1,-1):
+for x in range(1,(979//2-7-9)+1):
+    for y in range(1,(979//7-2-9)+1):
+        for z in range((979 // 9),0,-1):
             if (2 * x) + (7 * y) + (9 * z) == 979:
                 print(x, y, z)
                 exit()
